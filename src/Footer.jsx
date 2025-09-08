dd2d3419e20a00eb9f245c87a7653d2c4703a2f0
Kelemek <markdlarson@me.com>
2025-09-08 16:23:34 -0500

    imported assets into the project

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,14 @@
+import github from '../src/assets/github.svg'
+import linkedIn from '../src/assets/linkedin.svg'
+
 export default function Footer() {
     return (
         <footer>
             <a href="https://github.com/kelemek/" title="Link to Github Profile" target="_blank">
-                <img src="./assets/github.svg" alt="Github" />
+                <img src={github} alt="Github" />
             </a>
             <a href="https://www.linkedin.com/in/mark-larson-a33b3588" target="_blank">
-                <img src="./assets/linkedin.svg" title="Link to Linkedin Profile" alt="Linkedin" />
+                <img src={linkedIn} title="Link to Linkedin Profile" alt="Linkedin" />
             </a>
         </footer>
     )
--- src/Info.jsx
+++ src/Info.jsx
@@ -1,14 +1,18 @@
+import selfPortrait from '../src/assets/selfportrait.jpg'
+import linkedIn from '../src/assets/linkedin.svg'
+import email from '../src/assets/mail.svg'
+
 export default function Info() {
     return (
         <header>
             <nav>
-                <img src="./assets/selfportrait.jpg" alt="Self portrait of me, drawn by me" />
+                <img src={selfPortrait} alt="Self portrait of me, drawn by me" />
                 <h1>Mark Larson</h1>
                 <p>Frontend Developer</p>
                 <a href="https://marklarsonportfolio.netlify.app/">My Portfolio Website</a>
                 <div className="buttons">
-                    <a href="mailto:markdlarson@me.com" className="btn"><img src="./assets/mail.svg"/><span>Email</span></a>
-                    <a href="https://www.linkedin.com/in/mark-larson-a33b3588/" target="_blank" className="btn"><img src="./assets/linkedin.svg" /><span>LinkedIn</span></a>
+                    <a href="mailto:markdlarson@me.com" className="btn"><img src={email}/><span>Email</span></a>
+                    <a href="https://www.linkedin.com/in/mark-larson-a33b3588/" target="_blank" className="btn"><img src={linkedIn} /><span>LinkedIn</span></a>
                 </div>
             </nav>
         </header>
