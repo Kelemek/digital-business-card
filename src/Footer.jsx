b183a6f901a328a9a576805b2826a040e42c6923
Kelemek <markdlarson@me.com>
2025-09-08 17:13:48 -0500

    added portfolio

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,7 @@
 import github from '../src/assets/github.svg'
 import linkedIn from '../src/assets/linkedin.svg'
+import portfolio from '../src/assets/portfolio.svg'
+
 
 export default function Footer() {
     return (
@@ -9,6 +11,9 @@ export default function Footer() {
             </a>
             <a href="https://www.linkedin.com/in/mark-larson-a33b3588" target="_blank">
                 <img src={linkedIn} title="Link to Linkedin Profile" alt="Linkedin" />
+            </a>
+            <a href="https://marklarsonportfolio.netlify.app/" target="_blank">
+                <img src={portfolio} title="Link to my portfolio" alt="Portfolio" />
             </a>
         </footer>
     )
