fbbd2af046ff2298f6068d2a42a9ed05ed9d56c6
Kelemek <markdlarson@me.com>
2025-09-08 15:54:58 -0500

    updates to paths

--- src/Info.jsx
+++ src/Info.jsx
@@ -2,13 +2,13 @@ export default function Info() {
     return (
         <header>
             <nav>
-                <img src="../src/assets/selfportrait.jpg" alt="Self portrait of me, drawn by me" />
+                <img src="./assets/selfportrait.jpg" alt="Self portrait of me, drawn by me" />
                 <h1>Mark Larson</h1>
                 <p>Frontend Developer</p>
                 <a href="https://marklarsonportfolio.netlify.app/">My Portfolio Website</a>
                 <div className="buttons">
-                    <a href="mailto:markdlarson@me.com" className="btn"><img src="../src/assets/mail.svg"/><span>Email</span></a>
-                    <a href="https://www.linkedin.com/in/mark-larson-a33b3588/" target="_blank" className="btn"><img src="../src/assets/linkedin.svg" /><span>LinkedIn</span></a>
+                    <a href="mailto:markdlarson@me.com" className="btn"><img src="./assets/mail.svg"/><span>Email</span></a>
+                    <a href="https://www.linkedin.com/in/mark-larson-a33b3588/" target="_blank" className="btn"><img src="./assets/linkedin.svg" /><span>LinkedIn</span></a>
                 </div>
             </nav>
         </header>
